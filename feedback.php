943e754008d7c6fead23c37c994cb851afbd0e4e
black996 <rharasis@yahoo.com>
2022-05-20 21:47:35 +0300

    init project and extract header and footer

---
+++ feedback.php
@@ -0,0 +1,26 @@
+<?php include 'inc/header.php'; ?>
+<main>
+  <div class="container d-flex flex-column align-items-center">
+   
+    <h2>Feedback</h2>
+
+    <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+  </div>
+</main>
+<?php include 'inc/footer.php'; ?>
